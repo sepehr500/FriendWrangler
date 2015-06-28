eaf71e477c4f0cbe7ab675e7b70ea7ef8b7da2fa
Chris Zimmerman <chrisxzimmerman@gmail.com>
2015-06-28 12:25:09 -0400

    Added Tests Project

---
+++ FriendWranglerTests/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+
+namespace FriendWranglerTests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
