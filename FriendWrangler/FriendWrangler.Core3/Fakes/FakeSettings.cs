859c16f0079935258a99cfe5af5e0b192cd5175f
Chris Zimmerman <chrisxzimmerman@gmail.com>
2015-06-29 23:18:49 -0400

    Fixed directory path problems.

---
+++ FriendWrangler/FriendWrangler.Core3/Fakes/FakeSettings.cs
@@ -0,0 +1,23 @@
+﻿using FriendWrangler.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendWrangler.Core.Fakes
+{
+    public class FakeSettings : ISettings
+    {
+        public Models.User User { get; set; }
+
+        public void Save()
+        {
+           
+        }
+        public FakeSettings()
+        {
+
+        }
+    }
+}
---
+++ FriendWrangler/FriendWrangler.Core3/Models/Friend.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendWrangler.Core.Classes;
+using FriendWrangler.Core.Constants;
+
+namespace FriendWrangler.Core.Models
+{
+    public abstract class Friend
+    {
+          
+        #region Properties
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public InvitationLog InvitationLog { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        private Friend()
+        {
+
+        }
+
+        public Friend(string firstName, string lastName) : this (
+            firstName: firstName,
+            lastName: lastName,
+            emailAddress: FriendConstants.DEFAULT_EMAIL)
+        {
+
+        }
+
+        public Friend(string firstName, string lastName, string emailAddress)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.EmailAddress = emailAddress;
+        }
+
+        #endregion
+        
+    }
+}
