c0f43e10db45a923a2dbe82476302a981cc2c4bf
Sepher Sobhani <ssobhani@leidos.analytics.com>
2015-06-29 21:46:54 -0400

    Added sentiment

--- ClassLibrary1/Models/Sentiment.cs
+++ ClassLibrary1/Models/Sentiment.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 namespace FriendWrangler.Core.Models
 {
 
-public class Sentiment
-{
-public Probability probability { get; set; }
-public string label { get; set; }
-}
+    public class Sentiment
+    {
+        public Probability probability { get; set; }
+        public string label { get; set; }
+    }
 
-public class Probability
-{
-public float neg { get; set; }
-public float neutral { get; set; }
-public float pos { get; set; }
-}
+    public class Probability
+    {
+        public float neg { get; set; }
+        public float neutral { get; set; }
+        public float pos { get; set; }
+    }
 
-    
+
 }
