f9a1eb3183ec7121ea3e90f70348f551eb290628
Sepher Sobhani <ssobhani@leidos.analytics.com>
2015-06-29 23:37:30 -0400

    Added sentiment analysis

---
+++ FriendWranglerTests/ViewModels/AnalyzeSentimentTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendWrangler.Core.Models;
+using FriendWrangler.Core.Services;
+using FriendWrangler.Core.ViewModels;
+using NUnit.Framework;
+
+namespace FriendWranglerTests.ViewModels
+{
+    class AnalyzeSentimentTest
+    {
+
+        [TestFixture()]
+        public class LoginViewModelTests
+        {
+            public Invitation Invitation { get; set; }
+
+            [SetUp]
+            public void SetUp()
+            {
+                this.Invitation = new Invitation();
+            }
+
+            [Test()]
+            public async void AnalyzeSentiment()
+            {
+                var x = Invitation.AnalyzeSentiment("Hello My name is jim");
+                
+            }
+        }
+
+
+    }
+}
