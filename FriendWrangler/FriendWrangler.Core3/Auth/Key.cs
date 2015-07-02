0c0e3aff9421e891f8d47d61e70bd78d5f694472
Chris Zimmerman <chrisxzimmerman@gmail.com>
2015-07-02 19:53:58 -0400

    api keys. this will be redone later as it is incomplete!

---
+++ FriendWrangler/FriendWrangler.Core3/Auth/Key.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendWrangler.Core.Auth
+{
+    public class Key
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public string Service { get; set; }
+    }
+}
