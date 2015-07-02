c0cc75ce99d1794a6819fc4174c6d5f7b12d39e1
Sepher Sobhani <ssobhani@leidos.analytics.com>
2015-07-01 23:34:43 -0400

    Added Brodcast Receiver

--- FriendWrangler/FriendWrangler.Droid/Classes/AndroidFriend.cs
+++ FriendWrangler/FriendWrangler.Droid/Classes/AndroidFriend.cs
@@ -7,22 +7,26 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Telephony;
 using Android.Views;
 using Android.Widget;
 using FriendWrangler.Core.Models;
 
 namespace FriendWrangler.Droid.Classes
 {
-    class AndroidFriend : Friend
+    class AndroidFriend : Friend 
     {
+        private string PhoneNumber { get; set; }
         public override void SendInvitation(string message)
-        {
-            throw new NotImplementedException();
+        { 
+            SmsManager.Default.SendTextMessage(PhoneNumber , null , message , null ,null);
         }
 
         public override string ReceiveMessages()
         {
             throw new NotImplementedException();
         }
+
+
     }
 }
